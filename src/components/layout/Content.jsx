75f325a5dd08533174f74fc0868bcd86e5471560
husnidd1n.uz <husniddinhalimov112@gmail.com>
2025-04-14 15:14:41 +0500

    Features Section added

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import HeroSection from '../sections/HeroSection'
 import AboutUs from '../sections/AboutUs'
+import Features from '../sections/Features'
 
 const Content = () => {
   return (
     <div id='Content'>
       <HeroSection />
       <AboutUs />
+      <Features />
+
     </div>
   )
 }
---
+++ src/components/sections/Features.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../../assets/Features.css"
+const Features = () => {
+  return (
+    <div id='Features'>
+      <div className="container">
+        <div className="Features_info">
+          <h3>Save On Electricity Bills</h3>
+          <p>Generate your own clean energy from the sun for free with solar. Add Powerwall to store your energy for use anytime you need it. Tax incentives and flexible financing options may be available to help you get the best price for your solar system. By installing solar panels, you can also reduce your reliance on traditional energy sources.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Features
